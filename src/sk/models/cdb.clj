cff8a13f56754d0008b6c0b885c99b35f7089bc7
Hector Lucero <hectorqlucero@gmail.com>
2021-10-23 23:28:45 -0700

    Added needed tables and started eventos process

--- src/sk/models/cdb.clj
+++ src/sk/models/cdb.clj
@@ -2,7 +2,6 @@
   (:require [noir.util.crypt :as crypt]
             [sk.models.crud :refer [Insert-multi Query! db]]))
 
-
 ;; Start users table
 (def users-sql
   "CREATE TABLE users (
@@ -40,15 +39,33 @@
     :active "T"}])
 ;; End users table
 
+;; Start eventos
+(def eventos-sql
+  "
+  CREATE TABLE `eventos` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(200) DEFAULT NULL,
+  `detalles` text,
+  `lugar` text,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `organiza` varchar(100) DEFAULT NULL,
+  `imagen` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ")
+;; End eventos
+
 (defn drop-tables
   "Drops tables if they exist"
   []
-  (Query! db "DROP table IF EXISTS users"))
+  (Query! db "DROP table IF EXISTS users")
+  (Query! db "DROP table IF EXISTS eventos"))
 
 (defn create-tables
   "Creates tables"
   []
-  (Query! db users-sql))
+  (Query! db users-sql)
+  (Query! db eventos-sql))
 
 (defn populate-tables
   "Populates table with default data"
