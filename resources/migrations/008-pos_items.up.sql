9a3b58fa8873f25d87729ca4f247e31c34eb4003
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2024-10-18 13:03:13 -0700

    Added new table pos_items

---
+++ resources/migrations/008-pos_items.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE pos_items (
+  OID int(11) not null auto_increment primary key,
+  name varchar(200),
+  item_number varchar(200),
+  category varchar(200),
+  status_item int(1),
+  dt_registered datetime,
+  rfid_number varchar(300),
+  checkin_dt datetime,
+  checkout_dt datetime,
+  employee_checked_out varchar(200),
+  employee_checked_in varchar(200)
+) engine=innoDB default charset=utf8mb3;
